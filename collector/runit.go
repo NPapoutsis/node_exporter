782697195c3a76e4883e7e1610246e96708c6485
Johannes Ziemke <github@5pi.de>
2023-05-02 12:56:23 +0200

    Use go-runit fork, mark collector as deprecated
    Signed-off-by: Johannes Ziemke <github@5pi.de>

--- collector/runit.go
+++ collector/runit.go
@@ -18,10 +18,10 @@ package collector
 
 import (
 	"github.com/alecthomas/kingpin/v2"
+	"github.com/discordianfish/go-runit/runit"
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/soundcloud/go-runit/runit"
 )
 
 var runitServiceDir = kingpin.Flag("collector.runit.servicedir", "Path to runit service directory.").Default("/etc/service").String()
@@ -45,6 +45,8 @@ func NewRunitCollector(logger log.Logger) (Collector, error) {
 		constLabels = prometheus.Labels{"supervisor": "runit"}
 		labelNames  = []string{"service"}
 	)
+
+	level.Warn(logger).Log("msg", "This collector is deprecated and will be removed in the next major version release.")
 
 	return &runitCollector{
 		state: typedDesc{prometheus.NewDesc(
